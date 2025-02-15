664861027a1b4e0d0e829916284c4298ca63625f
Francis Poon <francool97@gmail.com>
2025-02-14 19:39:10 -0500

    Added basic button sound effects

--- pets/care_traits/care_button.gd
+++ pets/care_traits/care_button.gd
@@ -5,10 +5,14 @@ signal add_care(care_trait: Constants.CareTraitTypes, care_amount: float)
 
 @export var care_trait: Constants.CareTraitTypes
 @export var care_amount: float = 10
-
+@export var audio: AudioStream
+@export var volume: float
 
 func _ready() -> void:
 	pressed.connect(_on_pressed)
 
 func _on_pressed():
 	add_care.emit(care_trait, care_amount)
+	%SFXPlayer.stream = audio
+	%SFXPlayer.volume_db = volume
+	%SFXPlayer.play()
